alphagov/cheapseats

commit f76407e1d5777f47bf8b9f4249caa31eb873f5e5

Only download the dashboards we need
- If we're specifying just a single dashboard, then let's just get that dashboard from stagecraft

lib/spotlight.js
@@ -36,34 +36,46 @@ module.exports.init = function (config) {
   };
 };
 
+function downloadSingleDashboard(slug, path){
+  console.info('Downloading dashboard for:', slug);
+  return request(
+    'https://stagecraft.production.performance.service.gov.uk/public/dashboards?slug=' + slug)
+    .pipe(
+      fs.createWriteStream(path + slug + '.json')
+    );
+}
+
 function downloadDashboards(config) {
   var deferred = Q.defer(),
-      path = config.dashboardPath;
+      path = config.dashboardPath,
+      dashboardsRequest = config.dashboard ? config.dashboardList + '?slug=' + config.dashboard : config.dashboardList;
 
-  request(config.dashboardList, function (err, res, body) {
+  request(dashboardsRequest, function (err, res, body) {
     if (err) {
       deferred.reject(err);
     } else {
-      body = JSON.parse(body);
-      var total = body.items.length;
-      var count = 0;
       rimraf(path, function () {
         fs.mkdir(path, function () {
-          body.items.forEach(function (dashboard) {
-            console.log('Downloading dashboard for:', dashboard.slug);
-            request(config.dashboardList + '?slug=' + dashboard.slug)
-              .pipe(fs.createWriteStream(path + dashboard.slug + '.json'))
-              .on('error', function (err) {
-                deferred.reject(err);
-              })
-              .on('finish', function () {
+          if(config.dashboard){
+            downloadSingleDashboard(config.dashboard, path).on('finish', function () {
+              deferred.resolve();
+            });
+          } else {
+            var total = body.items.length;
+            var count = 0;
+
+            body = JSON.parse(body);
+            body.items.forEach(function (dashboard) {
+              var dashboardDL = downloadSingleDashboard(dashboard.slug, path);
+              dashboardDL.on('finish', function () {
                 count++;
 
                 if (count === total) {
                   deferred.resolve();
                 }
               });
-          });
+            });
+          }
         });
       });
     }

commit 1d8146fd7ae7c6a1ca56fcdaa5e37df6194c3b6a

Fix recently introduced bug
f76407e1d5777f47bf8b9f4249caa31eb873f5e5 broke testing many dashboards.

This fixes that.

lib/spotlight.js
@@ -61,10 +61,10 @@ function downloadDashboards(config) {
               deferred.resolve();
             });
           } else {
+            body = JSON.parse(body);
             var total = body.items.length;
             var count = 0;
 
-            body = JSON.parse(body);
             body.items.forEach(function (dashboard) {
               var dashboardDL = downloadSingleDashboard(dashboard.slug, path);
               dashboardDL.on('finish', function () {